d09c25b18aaf937e04ad65a33a7d7566094fd870
Dario Sindicic <dario.sindicic@gmail.com>
2018-03-07 15:42:46 +0100

    solved monad problem, constructor is working

--- src/Generator.hs
+++ src/Generator.hs
@@ -1,7 +1,6 @@
 module Generator where
 
-import System.Random (randomR, getStdRandom)
-import System.IO.Unsafe
+import System.Random (randomRIO)
 
 data Dot = Dot {xCor :: Double,
                 yCor :: Double}
@@ -17,22 +16,28 @@ generateDots = do
               newLine <- getLine
               numberOfDots <- getLine
               putStrLn $ "I will create " ++ show numberOfDots ++ " dots and save it to dots1.txt"
-              saveDots "dots1.txt" ( createDots ( read numberOfDots :: Int)) 
+              xs <- createDots (read numberOfDots :: Int)
+              ys <- createDots (read numberOfDots :: Int)
+              saveDots "dots1.txt" xs ys
               return ()
 
 
-createDots :: Int -> [Dot]
-createDots  0  = []
-createDots num =  [ 
-                  Dot ( unsafePerformIO (getStdRandom (randomR (-90.0 , 90.0))))
-                   (unsafePerformIO (getStdRandom (randomR (-180.0, 180.0))))
-                  ] ++ createDots (num-1)
+createDots :: Int -> IO([Double])
+createDots  0  = return []
+createDots num = do
+                  x <- randomRIO(-90.0 , 90.0)
+                  --y <- randomRIO(-180,0, 180.0)
+                  xs <- createDots (num-1)
+                  --dot <- Dot x y 
+                  return (x:xs)
                 
 
-saveDots :: FileName -> [Dot] -> IO ()
-saveDots filePath xs = writeFile filePath (dotsToString xs)
+saveDots :: FileName -> [Double] -> [Double] -> IO ()
+saveDots filePath xs ys = writeFile filePath (dotsToString xs ys)
 
-dotsToString :: [Dot] -> String
-dotsToString = foldl  (\acc x -> acc ++ getX x ++ "," ++ getY x ++"\n") ""
-            where getX = show . xCor 
-                  getY = show . yCor
+dotsToString :: [Double] -> [Double] -> String
+dotsToString [] [] = ""
+dotsToString (x:xs) (y:ys) = show x ++ "," ++ show y ++ "\n" ++ dotsToString xs ys
+--dotsToString = foldl  (\acc x -> acc ++ getX x ++ "," ++ getY x ++"\n") ""
+--            where getX = show . xCor 
+--                  getY = show . yCor
