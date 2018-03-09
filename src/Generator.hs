0db472076704939e132acdbd4aec01d370e56883
Dario Sindicic <dario.sindicic@gmail.com>
2018-03-09 20:48:10 +0100

    writeFile to appendFile

--- src/Generator.hs
+++ src/Generator.hs
@@ -16,28 +16,32 @@ generateDots = do
               newLine <- getLine
               numberOfDots <- getLine
               putStrLn $ "I will create " ++ show numberOfDots ++ " dots and save it to dots1.txt"
-              xs <- createDots (read numberOfDots :: Int)
-              ys <- createDots (read numberOfDots :: Int)
+              xs <- createDots (read numberOfDots :: Int)  (-90.0)   90.0
+              ys <- createDots (read numberOfDots :: Int) (-180.0)  180.0
               saveDots "dots1.txt" xs ys
               return ()
 
 
-createDots :: Int -> IO([Double])
-createDots  0  = return []
-createDots num = do
-                  x <- randomRIO(-90.0 , 90.0)
+createDots :: Int -> Double -> Double -> IO([Double])
+createDots  0    _   _ = return []
+createDots num min max = do
+                  x <- randomRIO(min , max)
                   --y <- randomRIO(-180,0, 180.0)
-                  xs <- createDots (num-1)
+                  xs <- createDots (num-1) min max
                   --dot <- Dot x y 
                   return (x:xs)
                 
 
 saveDots :: FileName -> [Double] -> [Double] -> IO ()
-saveDots filePath xs ys = writeFile filePath (dotsToString xs ys)
+saveDots filePath xs ys = do 
+                      writeFile filePath ""
+                      dotsToString xs ys filePath
+                      return ()
 
-dotsToString :: [Double] -> [Double] -> String
-dotsToString [] [] = ""
-dotsToString (x:xs) (y:ys) = show x ++ "," ++ show y ++ "\n" ++ dotsToString xs ys
---dotsToString = foldl  (\acc x -> acc ++ getX x ++ "," ++ getY x ++"\n") ""
---            where getX = show . xCor 
---                  getY = show . yCor
+
+dotsToString :: [Double] -> [Double] -> FileName -> IO ()
+dotsToString [] []  _ = return ()
+dotsToString (x:xs) (y:ys) filePath = do 
+                          appendFile filePath (show x ++ "," ++ show y ++ "\n")
+                          dotsToString xs ys filePath
+                          return ()
